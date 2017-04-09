d1911c84747bd93d7e78b36999282a52f48db884
Henry Tao <hi@henrytao.me>
2017-04-09 08:25:14 +0700

    Updated json

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
 const Shell = require('shelljs');
 
-const downloads = require('./db-001-050.json')['Video']
-const downloadDir = '~/Downloads/Dragon\\ Ball/Dragon\\ Ball/'
+const fill = (n) => {
+  n = parseInt(n)
+  return n < 10 ? `00${n}` : (n < 100 ? `0${n}` : n)
+}
 
-downloads.forEach((video, index) => {
-  const name = `Dragon\\ Ball\\ ${video.name.replace('db', '')}.mp4`
-  const source = video.source.sd
-  const command = `\ncurl ${source} -o ${downloadDir}${name}`
-  console.log(command)
-  Shell.exec(command)
+const files = [
+  'dbmovies.json'
+]
+files.forEach(filename => {
+  const downloads = require(`./${filename}`)
+  const downloadDir = '~/Downloads/Dragon\\ Ball/Dragon\\ Ball\\ Movies/'
+  downloads.forEach((video, index) => {
+    let name = video.name.replace(/ /g, '\\ ')
+    name = `${name}.mp4`
+    const source = video.url
+    const command = `\ncurl ${source} -o ${downloadDir}${name}`
+    console.log(command)
+    Shell.exec(command)
+  })
 })
+
+
+// const Shell = require('shelljs');
+
+// const fill = (n) => {
+//   n = parseInt(n)
+//   return n < 10 ? `00${n}` : (n < 100 ? `0${n}` : n)
+// }
+
+// const files = [
+//   'dbs-001-050.json',
+//   'dbs-051-100.json'
+// ]
+// files.forEach(filename => {
+//   const downloads = require(`./${filename}`)['Video']
+//   const downloadDir = '~/Downloads/Dragon\\ Ball/Dragon\\ Ball\\ Super/'
+//   downloads.forEach((video, index) => {
+//     let name = parseInt(video.name.replace('dbs', ''))
+//     if (name < 80) {
+//       return
+//     }
+//     name = `Dragon\\ Ball\\ Super\\ ${fill(name)}.mp4`
+//     const source = video.source.sd
+//     const command = `\ncurl ${source} -o ${downloadDir}${name}`
+//     console.log(command)
+//     Shell.exec(command)
+//   })
+// })
